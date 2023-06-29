d341fc5b8e6800bcac93ab65237be4fa4edc81ba
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-06-29 18:17:34 +0200

    add some style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,18 @@
     <body>
         <h1 class="text-center">Ciao da vite</h1>
         {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
-        <div>
+        <div class="container d-flex flex-wrap gap-3 p-4">
             @foreach ($movies as $movie)
-                <div>{{ $movie->title }}</div>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Original name - "{{ $movie->original_title }}"</h6>
+                    <div class="card-text">{{$movie->nationality}}</div>
+                    <div class="card-text">{{$movie->data}}</div>
+                    <div class="card-text">Vote: {{$movie->vote}}</div>
+                    
+                    </div>
+                </div>
             @endforeach
         </div>
     </body>
